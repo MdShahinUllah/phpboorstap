72eaa1adb56d258fe242a9fcc567056b4bc76f34
MdShahinUllah <shahinullah786@gmail.com>
2020-12-27 19:52:59 +0600

    mastaring

---
+++ app/Http/Controllers/testingController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class testingController extends Controller
+{
+    public function testing(){
+        return view('index');
+    }
+
+    public function register(){
+        return view('register');
+    }
+}
---
+++ resources/views/register.blade.php
@@ -0,0 +1,68 @@
+@extends('class5')
+@section('main')
+
+
+    <form>
+        <div style="width: 50%;margin: auto; background: lightcyan; padding: 30px;" >
+            <div class="form-row">
+                <div class="form-group ">
+                    <tr>
+                        <th><label for="name">Your name</label></th>
+                        <td><input type="text" class="form-control" id="name" placeholder="Your name"></td>
+                    </tr>
+
+
+                </div>
+                <div class="form-group ">
+                    <label for="inputEmail4">Email</label>
+                    <input type="email" class="form-control" id="inputEmail4" placeholder="Email">
+                </div>
+                <div class="form-group">
+                    <label for="inputPassword4">Password</label>
+                    <input type="password" class="form-control" id="inputPassword4" placeholder="Password">
+                </div>
+            </div>
+            <div class="form-group">
+                <label for="inputAddress">Address</label>
+                <input type="text" class="form-control" id="inputAddress" placeholder="1234 Main St">
+            </div>
+            <div class="form-group">
+                <label for="inputAddress2">Address 2</label>
+                <input type="text" class="form-control" id="inputAddress2" placeholder="Apartment, studio, or floor">
+            </div>
+            <div class="form-row">
+                <div class="form-group col-md-6">
+                    <label for="inputCity">City</label>
+                    <input type="text" class="form-control" id="inputCity">
+                </div>
+                <div class="form-group col-md-4">
+                    <label for="inputState">State</label>
+                    <select id="inputState" class="form-control">
+                        <option selected>Choose...</option>
+                        <option>Chittagong</option>
+                        <option>Dhaka</option>
+                    </select>
+                </div>
+                <div class="form-group col-md-2">
+                    <label for="inputZip">Zip</label>
+                    <input type="text" class="form-control" id="inputZip">
+                </div>
+            </div>
+            <div class="form-group">
+                <div class="form-check">
+                    <input class="form-check-input" type="checkbox" id="gridCheck">
+                    <label class="form-check-label" for="gridCheck">
+                        Check me out
+                    </label>
+                </div>
+            </div>
+            <button type="submit" class="btn btn-primary">Sign in</button>
+        </div>
+
+    </form>
+
+
+
+
+
+@stop
